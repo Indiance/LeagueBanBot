b1076b3a5653eda39c7691c9a49e3f34e6ee266d
Indiance <tharun.tvishy@gmail.com>
2022-02-04 20:06:14 +0530

    Get an array of the changed status

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // importing the necessary stuff
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Presence } = require('discord.js');
 const { discordtoken } = require('./config.json');
 
 // initialising the stuff
@@ -8,3 +8,26 @@ const client = new Client({ intents });
 
 // running the bot
 client.login(discordtoken);
+client.on("ready", () => {
+    client.user.setPresence({ activities: [{ name: 'Banning LoL players' }], status: 'online' });
+});
+
+/*
+Gameplan:
+check if league of legends is one of them
+if it is, ban
+Thats literally it
+*/
+
+client.on("presenceUpdate", (oldPresence, newPresence) => {
+	try {
+		activityNames = [];
+		newPresence.activities.forEach(activity => {
+			activityNames.push(activity.name);
+		});
+		console.log(activityNames);
+	}
+	catch {
+		console.log('undefined');
+	}
+})
